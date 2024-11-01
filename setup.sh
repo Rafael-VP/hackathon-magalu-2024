85bb1c6fb629c491554f21364e4a1cf1fdaa2d18
Rafael-VP <rafaelvpovoa@gmail.com>
2024-11-01 04:37:48 -0300

    Added status messages.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # Update and upgrade the system
+echo "Updating system..."
 sudo apt update -y
 sudo apt upgrade -y
 
 # Install necessary packages
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release git
-
+echo "Installing packages..."
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release git figlet
 sudo install -m 0755 -d /etc/apt/keyrings
 
 # Add Docker’s official GPG key
+echo "Installing Docker..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add the Docker repository to APT sources
@@ -26,6 +28,7 @@ sudo apt update -y
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Install Docker Compose
+echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
@@ -34,14 +37,16 @@ docker --version
 docker-compose --version
 
 # Clone the repository containing docker-compose files
+echo "Getting docker-compose.yml files..."
 git clone https://github.com/Rafael-VP/hackathon-magalu-2024.git ~/docker-services
 
 # Navigate to the directory with the docker-compose files
 cd ~/docker-services/docker
 
 # Create directories for configurations if necessary
-mkdir -p nextcloud/config pihole/config homer/config wireguard/config
+mkdir -p nextcloud/config pihole/config homer/config wireguard/config WooCommerce/config baikal/config
 
+echo "Deploying containers..."
 # Deploy NextCloud
 cd nextcloud
 sudo docker-compose up -d
@@ -58,8 +63,17 @@ sudo docker-compose up -d
 cd ../wireguard
 sudo docker-compose up -d
 
+# Deploy WooCommerce
+cd ../WooCommerce
+sudo docker-compose up -d
+
+# Deploy Baikal
+cd ../baikal
+sudo docker-compose up -d
+
+figlet Magalu Cloud
 echo "NextCloud, Pi-hole, Homer, and WireGuard have been set up and started."
 echo "Access NextCloud at http://<your-server-ip>:8080"
 echo "Access Pi-hole at http://<your-server-ip>"
 echo "Access Homer at http://<your-server-ip>:8081"
-
+echo "Ready to go!"
